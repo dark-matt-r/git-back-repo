7be928d86e56d3e6bcb6c374fafd88aaa432ce77
dark-matt-r <dark_matt_r@outlook.com>
2023-09-22 20:24:37 -0400

    Most javascript added

--- calculator.js
+++ calculator.js
@@ -1,4 +1,3 @@
-alert("Test");
 
 let operand1 = "0";
 let operand2 = null;
@@ -6,26 +5,42 @@ let operator = null;
 
 const display = document.querySelector(".display");
 
-
 /* Calculates and displays the final answer. */
 function calculate() {
+    let result = null;
     if ((operand1 === null) || (operand2 === null) || (operator === null)) {
-        display.innerHTML = "NO ANSWER";
+        result = "NO ANSWER";
+        clearDisplay();
+    } else if (operator === "/") {
+        result = Number.parseInt(operand1) / Number.parseInt(operand2);
+    } else if (operator === "x") {
+        result = Number.parseInt(operand1) * Number.parseInt(operand2);
+    } else if (operator === "-") {
+        result = Number.parseInt(operand1) - Number.parseInt(operand2);
+    } else if (operator === "+") {
+        result = Number.parseInt(operand1) + Number.parseInt(operand2);
     }
+    setDisplay(result);
     console.log(operand1, operator, operand2);
 }
 
 /* Clears the display element. */
 function clearDisplay() {
-    operand_1 = "0";
-    operand_2 = null;
+    operand1 = "0";
+    operand2 = null;
     operator = null;
+    setDisplay("0");
     console.log(operand1, operator, operand2);
+}
+
+function setDisplay(info) {
+    display.innerHTML = info;
 }
 
 /* Sets the operator variable. */
 function setOperator(newOperator) {
     operator = newOperator;
+    setDisplay(newOperator);
     console.log(operand1, operator, operand2);
 } 
 
@@ -33,8 +48,10 @@ function setOperator(newOperator) {
 function setOperands(digit) {
     if (operator === null) {
         setOperand1(digit);
+        setDisplay(operand1);
     } else {
         setOperand2(digit);
+        setDisplay(operand2);
     }
     console.log(operand1, operator, operand2);
 }
