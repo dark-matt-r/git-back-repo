a49133bfe4a80aed603376913c8902c46e670e9e
dark-matt-r <dark_matt_r@outlook.com>
2023-09-26 13:57:07 -0400

    More js

---
+++ word.js
@@ -0,0 +1,10 @@
+
+const wordUrl = "https://words.dev-apis.com/word-of-the-day";
+
+async function retrieveWord() {
+    const promise = await fetch(wordUrl);
+    const processedResponse = await promise.json();
+    return processedResponse;
+}
+
+console.log(retrieveWord());
